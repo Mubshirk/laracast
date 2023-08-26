d1ba6813a30d2c16fabc8ffa78eb0e6e450ca96e
mubashir <mubshirk875@gmail.com>
2023-08-26 12:46:26 +0500

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+    <article>
+        <h1> <a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate eveniet hic rerum, cupiditate nisi aliquam fuga minima deserunt similique dolore, qui eum corrupti. Suscipit unde voluptas, nesciunt reiciendis possimus praesentium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam quasi omnis explicabo ad esse natus, blanditiis a repellendus accusamus sed impedit totam aliquid rerum temporibus? Perspiciatis nihil impedit at dignissimos.
+        </p>
+
+    </article>
+
+     <article>
+        <h1> <a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate eveniet hic rerum, cupiditate nisi aliquam fuga minima deserunt similique dolore, qui eum corrupti. Suscipit unde voluptas, nesciunt reiciendis possimus praesentium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam quasi omnis explicabo ad esse natus, blanditiis a repellendus accusamus sed impedit totam aliquid rerum temporibus? Perspiciatis nihil impedit at dignissimos.
+        </p>
+        
+    </article>
+
+    <article>
+        <h1> <a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate eveniet hic rerum, cupiditate nisi aliquam fuga minima deserunt similique dolore, qui eum corrupti. Suscipit unde voluptas, nesciunt reiciendis possimus praesentium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam quasi omnis explicabo ad esse natus, blanditiis a repellendus accusamus sed impedit totam aliquid rerum temporibus? Perspiciatis nihil impedit at dignissimos.
+        </p>
+        
+    </article>
+
+    <article>
+        <h1> <a href="/post">My Fourth Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate eveniet hic rerum, cupiditate nisi aliquam fuga minima deserunt similique dolore, qui eum corrupti. Suscipit unde voluptas, nesciunt reiciendis possimus praesentium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam quasi omnis explicabo ad esse natus, blanditiis a repellendus accusamus sed impedit totam aliquid rerum temporibus? Perspiciatis nihil impedit at dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem obcaecati in aliquam commodi sunt culpa. Deleniti ab debitis repudiandae reiciendis, consectetur mollitia aspernatur porro. Laborum consequuntur dolore sapiente corrupti quos!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores labore, inventore id libero distinctio eos quae? Odio corrupti animi qui sed nemo ipsa laudantium maxime adipisci minima, perferendis nesciunt eligendi.
+        22
+        </p>
+        
+    </article>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
 
 Route::get('/dashboard', function () {
@@ -29,3 +29,12 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+////////////////////////////////
+
+Route::get('/post', function () {
+
+    return view('post');
+});
+
+
+
