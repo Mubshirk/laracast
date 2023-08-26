ed6f05ffdf7808523f20807d5e4f1dc630b0d287
mubashir <mubshirk875@gmail.com>
2023-08-26 12:46:07 +0500

    initail

---
+++ resources/views/post.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+
+    <article>
+        <h1>My First Post</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate eveniet hic rerum, cupiditate nisi aliquam fuga minima deserunt similique dolore, qui eum corrupti. Suscipit unde voluptas, nesciunt reiciendis possimus praesentium. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam quasi omnis explicabo ad esse natus, blanditiis a repellendus accusamus sed impedit totam aliquid rerum temporibus? Perspiciatis nihil impedit at dignissimos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius distinctio amet minima, dicta impedit animi debitis commodi doloribus esse labore consequatur fugit assumenda nihil est molestiae provident, dignissimos corporis.
+        </p>
+
+    </article>
+
+    <h3><a href="/"><-- Go Back</a></h3>
+
+    
+</body>
+</html>
